8e7c18f750683079359bbdb4b0f8485f43feccc7
Tolleiv Nietsch <tolleiv.nietsch@aoe.com>
2016-10-05 14:48:52 +0200

    Step 2 - DHCP options

--- 01_infrastructure_aws.tf
+++ 01_infrastructure_aws.tf
@@ -7,3 +7,16 @@ resource "aws_vpc" "main" {
     Name = "kubernetes"
   }
 }
+
+resource "aws_vpc_dhcp_options" "main" {
+  domain_name = "eu-central-1.compute.internal"
+  domain_name_servers = ["AmazonProvidedDNS"]
+  tags {
+    Name = "kubernetes"
+  }
+}
+
+resource "aws_vpc_dhcp_options_association" "dns_resolver" {
+  vpc_id = "${aws_vpc.main.id}"
+  dhcp_options_id = "${aws_vpc_dhcp_options.main.id}"
+}
