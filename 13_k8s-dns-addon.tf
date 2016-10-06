5735fc372d82f963af8f10c96d8e15f758604ad9
Tolleiv Nietsch <tolleiv.nietsch@aoe.com>
2016-10-06 17:07:27 +0200

    Step 13 - DNS addon

---
+++ 13_k8s-dns-addon.tf
@@ -0,0 +1,18 @@
+
+resource "null_resource" "provision-k8s-dns-addon" {
+  depends_on = [
+    "null_resource.provision-k8s-client"]
+
+  triggers {
+    instance_ips = "${aws_elb.kubernetes.id}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl create -f https://raw.githubusercontent.com/kelseyhightower/kubernetes-the-hard-way/master/services/kubedns.yaml
+kubectl --namespace=kube-system get svc
+kubectl create -f https://raw.githubusercontent.com/kelseyhightower/kubernetes-the-hard-way/master/deployments/kubedns.yaml
+kubectl --namespace=kube-system get pods
+    EOT
+  }
+}
